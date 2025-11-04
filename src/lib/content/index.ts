ebc363c9c50f2d81678d02463e518fb1efbaf6ed
Uncle Common <krishnerkar@gmail.com>
2025-11-04 19:44:00 +0000

    Group 2 generation complete

--- src/lib/content/index.ts
+++ src/lib/content/index.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
-	companyName: "Company",
-	description: "Description",
+	companyName: "Medfinder",
+	description: "Find your medications fast. We search 50+ pharmacies to locate in-stock prescriptions near you—saving you time and stress with our 99% success rate.",
 	image: "/og.png",
-	url: "https://example.com",
+	url: "https://trymedfinder.com",
 	fontFamily: "Inter Variable"
 };
